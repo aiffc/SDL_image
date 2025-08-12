921ad727111fe1e5d8d3d32a5b0536acb4c00db2
aiffc <yjc18915568435@gmail.com>
2025-08-12 10:43:06 +0800

    do build with zig

---
+++ build.zig
@@ -0,0 +1,96 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const min_version = "0.15.0";
+
+comptime {
+    const order = std.SemanticVersion.order;
+    const parse = std.SemanticVersion.parse;
+    if (order(builtin.zig_version, parse(min_ver) catch unreachable) == .lt)
+        @compileError("Raylib requires zig version " ++ min_ver);
+}
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(sdl_dep.artifact("SDL3"));
+
+    const sdl_image_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    sdl_image_mod.addCMacro("BUILD_SDL", "1");
+    sdl_image_mod.addCMacro("DLL_EXPORT", "1");
+    sdl_image_mod.addCMacro("LOAD_BMP", "1");
+    sdl_image_mod.addCMacro("LOAD_GIF", "1");
+    sdl_image_mod.addCMacro("LOAD_JPG", "1");
+    sdl_image_mod.addCMacro("LOAD_LBM", "1");
+    sdl_image_mod.addCMacro("LOAD_PCX", "1");
+    sdl_image_mod.addCMacro("LOAD_PNG", "1");
+    sdl_image_mod.addCMacro("LOAD_PNM", "1");
+    sdl_image_mod.addCMacro("LOAD_QOI", "1");
+    sdl_image_mod.addCMacro("LOAD_SVG", "1");
+    sdl_image_mod.addCMacro("LOAD_TGA", "1");
+    sdl_image_mod.addCMacro("LOAD_XCF", "1");
+    sdl_image_mod.addCMacro("LOAD_XPM", "1");
+    sdl_image_mod.addCMacro("LOAD_XV", "1");
+    sdl_image_mod.addCMacro("SAVE_BMP", "1");
+    sdl_image_mod.addCMacro("SAVE_GIF", "1");
+    sdl_image_mod.addCMacro("SAVE_JPG", "1");
+    sdl_image_mod.addCMacro("SAVE_PNG", "1");
+    sdl_image_mod.addCMacro("SAVE_TGA", "1");
+    sdl_image_mod.addCMacro("SDL_BUILD_MAJOR_VERSION", "3");
+    sdl_image_mod.addCMacro("SDL_BUILD_MICRO_VERSION", "0");
+    sdl_image_mod.addCMacro("SDL_BUILD_MINOR_VERSION", "3");
+    sdl_image_mod.addCMacro("USE_STBIMAGE", "1");
+
+    sdl_image_mod.addIncludePath(b.path("include/SDL3_image/"));
+    sdl_image_mod.addIncludePath(sdl_dep.namedLazyPath("SDL3/"));
+
+    const sdl_image_lib = b.addLibrary(.{
+        .name = "sdl_image",
+        .root_module = sdl_image_mod,
+    });
+
+    sdl_image_lib.addCSourceFiles(.{
+        .files = &.{
+            "src/IMG_anim_decoder.c",
+            "src/IMG_anim_encoder.c",
+            "src/IMG_avif.c",
+            "src/IMG_bmp.c",
+            "src/IMG_gif.c",
+            "src/IMG_jpg.c",
+            "src/IMG_jxl.c",
+            "src/IMG_lbm.c",
+            "src/IMG_libpng.c",
+            "src/IMG_pcx.c",
+            "src/IMG_png.c",
+            "src/IMG_pnm.c",
+            "src/IMG_qoi.c",
+            "src/IMG_stb.c",
+            "src/IMG_svg.c",
+            "src/IMG_tga.c",
+            "src/IMG_tif.c",
+            "src/IMG_webp.c",
+            "src/IMG_WIC.c",
+            "src/IMG_xcf.c",
+            "src/IMG_xpm.c",
+            "src/IMG_xv.c",
+            "src/IMG_xxx.c",
+            "src/IMG.c",
+        },
+        .flags = &.{},
+    });
+
+    sdl_image_lib.linkLibrary(sdl_dep.artifact("SDL3"));
+    sdl_image_lib.installHeader(b.path("include/SDL3_image/SDL_image.h"), "SDL3_image/SDL_image.h");
+    b.installArtifact(sdl_image_lib);
+}
